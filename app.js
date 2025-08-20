9ddd0b0b3361591a872963d81dade616ba296a24
Yohannes <yohannes.kelemeworki@gmail.com>
2025-08-20 21:01:20 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+// Enkel alert när man trycker på "Köp nu"
+document.querySelector(".hero button").addEventListener("click", () => {
+  alert("Välkommen till Marocko Kakel – vår webbshop öppnar snart!");
+});
